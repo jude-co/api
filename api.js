3b16333abf8aa27c079402a9395a9241d71ed78b
jude-co <atalobhor@gmail.com>
2021-04-29 16:05:58 +0100

    second commit

--- api.js
+++ api.js
@@ -11,8 +11,21 @@ document.getElementById('button6').addEventListener('click', Users);*/
     .then(response => response.json())
     .then(data => {
          console.log(data)
-         document.getElementById('button1').innerHTML = JSON.stringify(data);
-         });   
+         let posts = '';
+                                     data.forEach(element => {
+                                       posts += `<div class='card'><ul> 
+                                       <li>${element.body}</li>
+                                       <li>${element.id}</li>
+                                       <li>${element.title}</li>
+                                       <li>${element.userId}</li>
+
+                                       </ul>
+
+                                       </div>`
+         document.getElementById('button1').innerHTML = posts;
+         });  
+         
+      });
      }
      
 
@@ -21,8 +34,22 @@ document.getElementById('button6').addEventListener('click', Users);*/
         .then((response) => response.json())
         .then((data) => {
              console.log(data)
-             document.getElementById('button2').innerHTML = data;
+             let comments = '';
+                                     data.forEach(element => {
+                                       comments += `<div class='card'><ul> 
+                                       <li>${element.body}</li>
+                                       <li>${element.email}</li>
+                                       <li>${element.id}</li>
+                                       <li>${element.name}</li>
+                                       <li>${element.postId}</li>
+
+                                       </ul>
+
+                                       </div>`
+             document.getElementById('button2').innerHTML = comments;
              });
+
+            });
           }
 
 
@@ -31,8 +58,21 @@ document.getElementById('button6').addEventListener('click', Users);*/
                 .then(response => response.json())
                 .then(data => {
                      console.log(data)
-                     document.getElementById('button3').innerHTML = data;
+                     let albums = '';
+                                     data.forEach(element => {
+                                       albums += `<div class='card'><ul> 
+                                       <li>${element.id}</li>
+                                       <li>${element.title}</li>
+                                       <li>${element.userId}</li>
+                                 
+                                       </ul>
+
+                                       </div>`
+                     document.getElementById('button3').innerHTML = albums;
                      });
+
+                  });
+
                     }
 
 
@@ -41,8 +81,23 @@ document.getElementById('button6').addEventListener('click', Users);*/
                         .then(response => response.json())
                         .then(data => {
                              console.log(data)
-                             document.getElementById('button4').innerHTML = data;
+                             console.log(data)
+                                     let photos = '';
+                                     data.forEach(element => {
+                                       photos += `<div class='card'><ul> 
+                                       <li>${element.albumId}</li>
+                                       <li>${element.id}</li>
+                                       <img src="https://via.placeholder.com/150/92c952">
+                                       <li>${element.title}</li>
+                                       <li>${element.url}</li>
+
+                                       </ul>
+
+                                       </div>`
+                             document.getElementById('button4').innerHTML = photos;
                              });
+
+                           });
                          }
 
 
@@ -51,8 +106,22 @@ document.getElementById('button6').addEventListener('click', Users);*/
                                 .then(response => response.json())
                                 .then(data => {
                                      console.log(data)
-                                     document.getElementById('button5').innerHTML = data;
+                                     let todos = '';
+                                     data.forEach(element => {
+                                       todos += `<div class='card'><ul> 
+                                       <li>${element.completed}</li>
+                                       <li>${element.id}</li>
+                                       <li>${element.title}</li>
+                                       <li>${element.userId}</li>
+                                 
+                                       </ul>
+
+                                       </div>`
+                                     document.getElementById('button5').innerHTML = todos;
                                      });
+
+                                    });
+
                                    }
 
 
@@ -61,7 +130,26 @@ document.getElementById('button6').addEventListener('click', Users);*/
                                         .then(response => response.json())
                                         .then(data => {
                                              console.log(data)
-                                             document.getElementById('button6').innerHTML = JSON.stringify(data);
+                                             let users = '';
+                                             data.forEach(element => {
+                                                users += `<div class='card'><ul> 
+                                                <li>${element.name}</li>
+                                                <li>${element.phone}</li>
+                                                <li>${element.website}</li>
+                                                <li>${element.username}</li>
+                                                <li>${element.email}</li>
+                                                <li>${element.address.street}</li>
+                                                <li>${element.company.name}</li>
+                                                
+
+                                                </ul>
+
+                                                </div>`
+                                                document.getElementById('button6').innerHTML = users;
+                                             });
+
+
+                                          
                                              });
                                         }
 
